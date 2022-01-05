9bf34e4b75995f6cf6e9bf47acea443a90edc074
Alex <dev-worlds@yandex.ru>
2022-01-05 17:42:49 +0300

    add login page, add base chat view page

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,90 @@
 <template>
   <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <v-btn @click="message">NEW MESSAGE</v-btn>
+    <v-flex xs12 sm8>
+      <v-card min-width="400">
+        <v-card-title><h1>Nuxt чат</h1></v-card-title>
+        <v-card-text>
+          <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+            @submit.prevent="submit"
+          >
+            <v-text-field
+              v-model="name"
+              :counter="16"
+              :rules="nameRules"
+              label="Ваше имя"
+              required
+            ></v-text-field>
+
+            <v-text-field
+              v-model="room"
+              :rules="roomRules"
+              label="Введите комнату"
+              required
+            ></v-text-field>
+
+            <v-btn
+              :disabled="!valid"
+              color="primary"
+              class="mr-4"
+              type="submit"
+            >
+              Войти
+            </v-btn>
+
+          </v-form>
+        </v-card-text>
+      </v-card>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
+import {mapMutations} from 'vuex'
+
 export default {
+  layout: 'empty',
+  head: {
+    title: 'Добро пожаловать в nuxt чат'
+  },
   sockets: {
-    connect: function() {
-      console.log("socket connected");
+    connect: function () {
+      console.log('socket connected')
     }
   },
+  data: () => ({
+    valid: true,
+    name: '',
+    nameRules: [
+      v => !!v || 'Введите имя',
+      v => (v && v.length <= 16) || 'Имя не должно превышать 16 символов',
+    ],
+    room: '',
+    roomRules: [
+      v => !!v || 'Введите комнату'
+    ],
+  }),
+
   methods: {
-    message() {
-      this.$socket.emit("createMessage", {
-        text: "FROM CLIENT"
-      });
-    }
-  }
-};
+    ...mapMutations(['setUser']),
+    submit(e) {
+      if (this.$refs.form.validate()) {
+        const user = {
+          name: this.name,
+          room: this.room
+        }
+        this.setUser(user)
+        this.$router.push('/chat')
+      }
+    },
+    reset() {
+      this.$refs.form.reset()
+    },
+    resetValidation() {
+      this.$refs.form.resetValidation()
+    },
+  },
+}
 </script>
