5bdbd467acf652453e1c7ee8ef922c1539dfb9d7
Alex <dev-worlds@yandex.ru>
2022-01-06 05:26:50 +0300

    change domain

--- plugins/socket.js
+++ plugins/socket.js
@@ -5,7 +5,7 @@ export default function({ store }) {
   Vue.use(
     new VueSocketIO({
       debug: false,
-      connection: 'http://localhost:3000',
+      connection: 'http://dev-worlds.ru',
       vuex: {
         store,
         actionPrefix: 'SOCKET_',
